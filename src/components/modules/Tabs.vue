041ef37e452072c931c402fabdfd5e547a75a305
Nathalia <nathaliawitdolatko@gmail.com>
2025-06-07 22:13:02 +0200

    Kommentar til tabs og slugs

--- src/components/modules/Tabs.vue
+++ src/components/modules/Tabs.vue
@@ -1,33 +1,39 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+// Definere hvilke properties
 const props = defineProps<{
   about: string[]
   access: string
   terms: string
 }>()
 
+// Opretter en reaktiv variabel som holder styr på hvilken tab er aktiv
 const activeTab = ref('about')
 </script>
 
 <template>
-  <div class="tabs">
+  <article class="tabs">
+     <!-- Når knapperne trykkes på, skifter de aktiv fane -->
     <div class="tab-buttons">
       <button :class="{ 'active-tab': activeTab === 'about' }" @click="activeTab = 'about'">Om</button>
       <button :class="{ 'active-tab': activeTab === 'access' }" @click="activeTab = 'access'">Adgang</button>
       <button :class="{ 'active-tab': activeTab === 'terms' }" @click="activeTab = 'terms'">Betingelser</button>
     </div>
 
+    <!-- Viser kun indholdet for den aktive tab -->
     <div class="tab-content" v-show="activeTab === 'about'">
+      <!-- Går igennem alle tekster i arrayet i Strapi og viser dem -->
       <p v-for="(paragraph, i) in props.about" :key="i">{{ paragraph }}</p>
     </div>
+     <!-- Viser indhold fra access når den er aktiv -->
     <div class="tab-content" v-show="activeTab === 'access'">
       <p>{{ props.access }}</p>
     </div>
     <div class="tab-content" v-show="activeTab === 'terms'">
       <p>{{ props.terms }}</p>
     </div>
-  </div>
+  </article>
 </template>
 
 <style scoped>

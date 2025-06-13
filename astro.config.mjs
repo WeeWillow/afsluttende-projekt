dd85cbc79713cce37b96d44f1fb69b0142015d0a
Willow <98649380+WeeWillow@users.noreply.github.com>
2025-06-13 12:53:46 +0200

    vite config flyttet til astro config
    igen med formål at linux og vscode skal samarbejde, ikke relateret til projektet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/cases/opdagdanmark-v2/',
   output: "static",
-  integrations: [vue()]
+  integrations: [vue()],
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 500,
+        ignored: [
+          '**/node_modules/**',
+          '**/.git/**',
+          '**/dist/**',
+          '**/.astro/**',
+          '**/.cache/**',
+        ],
+      },
+    },
+  },
 });
